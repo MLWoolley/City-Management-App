777cd6d4a33b432b4726923d453393941945ef63
MLWoolley <103423434+MLWoolley@users.noreply.github.com>
2024-10-29 12:14:15 -0400

    Added method in main to build tree from csv file.

--- City Management App/main.cpp
+++ City Management App/main.cpp
@@ -1,20 +1,37 @@
 #include "BinarySearchTree.h"
 #include "City.h"
 #include "menu.h"
+#include "utilities.h"
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-/** Program description
+/** City Management App allows the user to access, search and alter city data from the "USCities-100-1.csv" file. City information is stored as City objects in a Binary Search Tree.
 * @author Madeleine Woolley
 */
 
 //prototype methods
+void buildTree(BinarySearchTree<City>& cityTree, string& filename);
 
-
-int main()
-{
+int main() {
     BinarySearchTree<City> cityTree;
-    //write csv file data to cityTree by passing lines to City constructor
+    string filename = "USCities-100-1.csv";
+    buildTree(cityTree, filename);
+    cout << "Working!" << endl;
     mainMenu(cityTree);
+}
+
+void buildTree(BinarySearchTree<City>& cityTree, string& filename) {
+    ifstream infile(filename);
+    util::checkFileOpen(infile, filename);
+
+    string line = "";
+    getline(infile, line); //skip headers
+    while (getline(infile, line)) {
+        City newCity(line);
+        cityTree.insert(newCity);
+    }
+    infile.close();
 }
